ebfd4cbc0df7730efbd369bf0204550f87b27fb1
Mayukh Das <mayukhdasbloodofseven@gmail.com>
2026-07-12 02:22:02 +0600

    seventh commit

--- src/config.py
+++ src/config.py
@@ -1,16 +1,16 @@
 import os
 
-BASE_PATH = os.path.join(os.path.expanduser("~"), "Downloads")
+BASE_PATH = r"C:\Users\Mayukh Das\Downloads"
 
 DESTINATIONS = {
-    "images": os.path.join(os.path.expanduser("~"), "Pictures"),
-    "documents": os.path.join(os.path.expanduser("~"), "Documents"),
-    "media": os.path.join(os.path.expanduser("~"), "Videos"),
-    "music": os.path.join(os.path.expanduser("~"), "Music")
+    "images": r"C:\Users\Mayukh Das\Pictures",
+    "documents": r"C:\Users\Mayukh Das\Documents",
+    "media": r"C:\Users\Mayukh Das\Videos",
+    "music": r"C:\Users\Mayukh Das\Music"
 }
 
 FILE_CATEGORIES = {
-    "images": [".jpg", ".jpeg", ".png", ".gif", ".svg"],
+    "images": [".jpg", ".jpeg", ".png", ".gif"],
     "documents": [".pdf", ".docx", ".txt", ".xlsx", ".pptx"],
     "media": [".mp4", ".mov", ".avi"],
     "music": [".mp3", ".wav", ".flac"]
